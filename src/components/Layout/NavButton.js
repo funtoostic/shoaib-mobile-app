2bf5106dd77d8b92d668230b09d8b199c62ce97c
M Afzaal Afzal <afzaal1721@gmail.com>
2021-06-21 13:48:23 +0500

    Icon bug fixed

--- src/components/Layout/NavButton.js
+++ src/components/Layout/NavButton.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Button, forwardRef, Text, useMediaQuery} from "@chakra-ui/react";
+import {Box, Button, forwardRef, Text, useMediaQuery} from "@chakra-ui/react";
 
-const MyComponent  = forwardRef  (({icon,heading,...props},ref) => {
+const MyComponent = forwardRef(({icon, heading, ...props}, ref) => {
 
     const [isLargerThan400] = useMediaQuery("(min-width: 400px)")
 
     return (
-        <Button fontWeight={'normal'} variant={'unstyled'} cursor={'pointer'} ref={ref} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} {...props}>
-            {icon}
-            <Text mt={1} fontSize={isLargerThan400 ? '1rem' : '13px'} color={'#fff'}>{heading}</Text>
+        <Button fontWeight={'normal'} variant={'unstyled'} cursor={'pointer'} ref={ref}  {...props}>
+            <Box display={'flex'}
+                 flexDirection={'column'} justifyContent={'center'} alignItems={'center'}
+            >
+                {icon}
+                <Text mt={1} fontSize={isLargerThan400 ? '1rem' : '13px'} color={'#fff'}>{heading}</Text>
+            </Box>
         </Button>
     );
 })
