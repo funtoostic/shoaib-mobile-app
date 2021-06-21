643fa61aabc62c71335a78362a38355b4a6baf6c
Ravichandran <ravi.nila@live.com>
2021-06-22 02:10:07 +0530

    fixes

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,262 +1,248 @@
-import React, {useState} from 'react';
-import {Box, Container, Heading, HStack, Spacer, Text, useMediaQuery, useToast} from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Box,
+  Container,
+  Heading,
+  HStack,
+  Spacer,
+  Text,
+  useMediaQuery,
+  useToast,
+} from "@chakra-ui/react";
 import BidTimeSection from "../../src/components/home/BidTiimeSection/BidTimeSection";
-import {IoIosArrowDropleftCircle} from "react-icons/io";
-import Link from 'next/link'
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+import Link from "next/link";
 import Image from "next/image";
-import {IoArrowForwardCircleOutline} from "react-icons/io5";
-import {client} from "../../src/utils/utils";
+import { IoArrowForwardCircleOutline } from "react-icons/io5";
+import { client } from "../../src/utils/utils";
 import parse from "html-react-parser";
 import WhiteButton from "../../src/components/Buttons/WhiteButton";
 
-const ProductDetails = ({productData,pointsData,id}) => {
+const ProductDetails = ({ productData, pointsData, id }) => {
+  const [isLargerThan480] = useMediaQuery("(min-width: 480px)");
 
-    const [isLargerThan480] = useMediaQuery("(min-width: 480px)")
+  const toast = useToast();
 
-    const toast = useToast();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [isLoading,setIsLoading] = useState(false);
+  const placeBidHandler = () => {
+    // if the user has less balance then it will return
+    if (pointsData.balance < productData.minimumBid) {
+      toast({
+        title: "Error",
+        description: "Insufficient Funds",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
 
-    const placeBidHandler = () => {
-
-        // if the user has less balance then it will return
-        if (pointsData.balance < productData.minimumBid) {
-
-            toast({
-                title: "Error",
-                description: "Insufficient Funds",
-                status: "error",
-                duration: 9000,
-                isClosable: true,
-            })
-
-            return;
-        }
-
-        // if the user has balance then the logic goes here
-        setIsLoading(true);
-
-        client.post(`/v1/reward/${id}/bid`,{
-
-            bidValue: productData.minimumBid,
-
-        })
-            .then(() => {
-
-                setIsLoading(false);
-
-                toast({
-                    title: "Success",
-                    description: "Bid Successful",
-                    status: "success",
-                    duration: 9000,
-                    isClosable: true,
-                })
-
-            })
-            .catch((err)=> {
-
-                setIsLoading(false);
-
-                toast({
-                    title: "Error",
-                    description: err.message,
-                    status: "error",
-                    duration: 9000,
-                    isClosable: true,
-                })
-
-            })
+      return;
     }
 
-    console.log(productData)
+    // if the user has balance then the logic goes here
+    setIsLoading(true);
 
-    return (
-        <Box minH={'100vh'} pb={'5rem'}>
+    client
+      .post(`/v1/reward/${id}/bid`, {
+        bidValue: productData.minimumBid,
+      })
+      .then(() => {
+        setIsLoading(false);
 
-            <Container px={0} maxW={'container.md'}>
+        toast({
+          title: "Success",
+          description: "Bid Successful",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+      })
+      .catch((err) => {
+        setIsLoading(false);
 
-                {/* Image section*/}
-                <Box mx={'auto'}>
-                    <Box
-                        mt={'-4.5rem'}
-                        bg={'transparent'}
-                        display={'flex'}
-                        justifyContent={'center'}
-                        w={'100%'}
-                        pos={'relative'}
-                    >
-                        <Image
-                            src={productData.details.productBanner}
-                            priority={'true'}
-                            placeholder={'blur'}
-                            width={'568'}
-                            height={'824'}
-                            layout={'intrinsic'}
-                            objectFit={'contain'}
+        toast({
+          title: "Error",
+          description: err.message,
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      });
+  };
 
-                        />
+  console.log(productData);
 
-                        <Box px={4} position={'absolute'} top={['60px','80px','110px']} left={0} width={'100%'}>
-                            <HStack>
-                                <Box as={Link} href={'/reward'}>
-                                    <Box cursor={'pointer'}>
-                                        <IoIosArrowDropleftCircle size={20}/>
-                                    </Box>
-                                </Box>
-                                <Box flexGrow={1}>
-                                    <BidTimeSection/>
-                                </Box>
-                            </HStack>
-                        </Box>
+  return (
+    <Box minH={"100vh"} pb={"5rem"}>
+      <Container px={0} maxW={"container.md"}>
+        {/* Image section*/}
+        <Box mx={"auto"}>
+          <Box
+            mt={"-4.5rem"}
+            bg={"transparent"}
+            display={"flex"}
+            justifyContent={"center"}
+            w={"100%"}
+            pos={"relative"}
+          >
+            <Image
+              src={productData.details.productBanner}
+              priority={"true"}
+              placeholder={"blur"}
+              width={"568"}
+              height={"824"}
+              layout={"intrinsic"}
+              objectFit={"contain"}
+            />
 
-                        {/*    Description section*/}
-                        <Box
-                            w={'100%'}
-                            pos={'absolute'}
-                            top={'88%'}
-                            mb={'-2rem'}
-                            p={4}
-                            zIndex={60}
-                            bg={'#fff'}
-                            borderRadius={'15px 15px 0 0'}
-                            pb={['13rem','14rem','15rem']}
-                        >
-                            {/*<HStack>*/}
-                                {/*left box*/}
-                                <Box>
-                                    <Heading fontSize={'20px'} as={'h3'}>
-                                        {productData.details.title}
-                                    </Heading>
-                                    <Text fontSize={'1rem'}>
-                                        {productData.details.subTitle}
-                                    </Text>
-                                </Box>
-
-                                {/*<Spacer/>*/}
-
-                                {/*/!*right box*!/*/}
-                                {/*<Box>*/}
-                                {/*    <Heading fontSize={'20px'} as={'h3'}>*/}
-                                {/*        ₹ 9,999*/}
-                                {/*    </Heading>*/}
-                                {/*    <Text fontSize={'1rem'}>*/}
-                                {/*        48MP | 6000mAh*/}
-                                {/*    </Text>*/}
-                                {/*</Box>*/}
-
-                            {/*</HStack>*/}
-
-                            {/*    Desc Text*/}
-                            <Box my={4}>
-                                {parse(productData.details.description)}
-                            </Box>
-
-                            <Heading fontSize={'20px'} as={'h3'}>
-                                Rules to participate
-                            </Heading>
-
-                            <Box  mt={4}>
-                                {parse(productData.details.rules)}
-                            </Box>
-
-                        </Box>
-
-                    </Box>
+            <Box
+              px={4}
+              position={"absolute"}
+              top={["60px", "80px", "110px"]}
+              left={0}
+              width={"100%"}
+            >
+              <HStack>
+                <Box as={Link} href={"/rewards"}>
+                  <Box cursor={"pointer"}>
+                    <IoIosArrowDropleftCircle size={20} />
+                  </Box>
                 </Box>
-
-                {/*    Fixed Button*/}
-
-                <Box
-                    width={'100%'}
-                    bottom={['95','100','110px']}
-                    pos={'fixed'}
-                    p={4}
-                    zIndex={100}
-
-
-                >
-                    <HStack
-                        p={4}
-                        bg={'dark.500'}
-                        borderRadius={'lg'}
-                        mx={'auto'}
-                    >
-
-                        <Box display={'flex'}>
-
-                            <Box cursor={'pointer'}>
-                                <Box as={Link} href={'/'}>
-                                    <img src={'/header/b.svg'} alt="b"
-                                         style={{
-                                             widows: 'auto',
-                                             height: `${isLargerThan480 ? "50px" : "40px"}`,
-                                         }}
-                                    />
-                                </Box>
-                            </Box>
-
-                            <Box px={4}>
-                                <Text fontWeight={'normal'} fontSize={['1rem']}>
-                                    User Points
-                                </Text>
-                                <Text letterSpacing={'2px'} color={'white'} fontWeight={'bold'}
-                                      fontSize={['12px']}>
-                                    {pointsData.balance}
-                                </Text>
-                            </Box>
-
-                        </Box>
-
-                        <Spacer/>
-                        <Box>
-                            <WhiteButton
-                                isLoading={isLoading}
-                                onClick={placeBidHandler}
-                                fontSize={'12px'}
-                                size={'sm'}
-                                rightIcon={<IoArrowForwardCircleOutline/>}
-                            >
-                                Place Bid
-                            </WhiteButton>
-                        </Box>
-
-                    </HStack>
-
+                <Box flexGrow={1}>
+                  <BidTimeSection />
                 </Box>
+              </HStack>
+            </Box>
 
+            {/*    Description section*/}
+            <Box
+              w={"100%"}
+              pos={"absolute"}
+              top={"88%"}
+              mb={"-2rem"}
+              p={4}
+              zIndex={60}
+              bg={"#fff"}
+              borderRadius={"15px 15px 0 0"}
+              pb={["13rem", "14rem", "15rem"]}
+            >
+              {/*<HStack>*/}
+              {/*left box*/}
+              <Box>
+                <Heading fontSize={"20px"} as={"h3"}>
+                  {productData.details.title}
+                </Heading>
+                <Text fontSize={"1rem"}>{productData.details.subTitle}</Text>
+              </Box>
 
-            </Container>
+              {/*<Spacer/>*/}
 
+              {/*/!*right box*!/*/}
+              {/*<Box>*/}
+              {/*    <Heading fontSize={'20px'} as={'h3'}>*/}
+              {/*        ₹ 9,999*/}
+              {/*    </Heading>*/}
+              {/*    <Text fontSize={'1rem'}>*/}
+              {/*        48MP | 6000mAh*/}
+              {/*    </Text>*/}
+              {/*</Box>*/}
+
+              {/*</HStack>*/}
+
+              {/*    Desc Text*/}
+              <Box my={4}>{parse(productData.details.description)}</Box>
+
+              <Heading fontSize={"20px"} as={"h3"}>
+                Rules to participate
+              </Heading>
+
+              <Box mt={4}>{parse(productData.details.rules)}</Box>
+            </Box>
+          </Box>
         </Box>
-    );
+
+        {/*    Fixed Button*/}
+
+        <Box
+          width={"100%"}
+          bottom={["95", "100", "110px"]}
+          pos={"fixed"}
+          p={4}
+          zIndex={100}
+        >
+          <HStack p={4} bg={"dark.500"} borderRadius={"lg"} mx={"auto"}>
+            <Box display={"flex"}>
+              <Box cursor={"pointer"}>
+                <Box as={Link} href={"/"}>
+                  <img
+                    src={"/header/b.svg"}
+                    alt="b"
+                    style={{
+                      widows: "auto",
+                      height: `${isLargerThan480 ? "50px" : "40px"}`,
+                    }}
+                  />
+                </Box>
+              </Box>
+
+              <Box px={4}>
+                <Text fontWeight={"normal"} fontSize={["1rem"]}>
+                  User Points
+                </Text>
+                <Text
+                  letterSpacing={"2px"}
+                  color={"white"}
+                  fontWeight={"bold"}
+                  fontSize={["12px"]}
+                >
+                  {pointsData.balance}
+                </Text>
+              </Box>
+            </Box>
+
+            <Spacer />
+            <Box>
+              <WhiteButton
+                isLoading={isLoading}
+                onClick={placeBidHandler}
+                fontSize={"12px"}
+                size={"sm"}
+                rightIcon={<IoArrowForwardCircleOutline />}
+              >
+                Place Bid
+              </WhiteButton>
+            </Box>
+          </HStack>
+        </Box>
+      </Container>
+    </Box>
+  );
 };
 
-export async function getServerSideProps({query: {id}}) {
+export async function getServerSideProps({ query: { id } }) {
+  //here we are using axios
+  const resProduct = await client.get(`/v1/reward/${id}`);
+  const resPoints = await client.get("/v1/point");
 
-    //here we are using axios
-    const resProduct = await client.get(`/v1/reward/${id}`)
-    const resPoints = await client.get('/v1/point')
+  const pointsData = await resPoints.data;
+  const productData = await resProduct.data;
 
-    const pointsData = await resPoints.data;
-    const productData = await resProduct.data;
-
-    if (!productData ) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        }
-    }
-
+  if (!productData) {
     return {
-        props: {
-            productData,
-            pointsData,
-            id
-        },
-    }
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      productData,
+      pointsData,
+      id,
+    },
+  };
 }
 
 export default ProductDetails;
--- src/components/home/ImageWithCTABtn.js
+++ src/components/home/ImageWithCTABtn.js
@@ -1,60 +1,72 @@
-import React, { useState} from 'react';
-import {Box} from "@chakra-ui/react";
-import Image from 'next/image'
-import {IoArrowForwardCircleOutline} from "react-icons/io5";
+import React, { useState } from "react";
+import { Box } from "@chakra-ui/react";
+import Image from "next/image";
+import { IoArrowForwardCircleOutline } from "react-icons/io5";
 import WhiteButton from "../Buttons/WhiteButton";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
-const ImageWithCTABtn = ({imageSrc, id}) => {
+const ImageWithCTABtn = ({ imageSrc, id, title }) => {
+  const router = useRouter();
 
-    const router = useRouter();
+  const [isLoading, setIsLoading] = useState();
 
-    const [isLoading, setIsLoading] = useState();
+  const buttonClickHandler = () => {
+    setIsLoading(true);
 
+    router.push(`product/${id}`);
+  };
 
-    const buttonClickHandler = () => {
+  return (
+    <Box
+      mx={"auto"}
+      bg={"transparent"}
+      display={"flex"}
+      justifyContent={"center"}
+      w={"96%"}
+      pos={"relative"}
+    >
+      <Image
+        src={imageSrc}
+        priority={"true"}
+        placeholder={"blur"}
+        width={"568"}
+        height={"824"}
+        layout={"intrinsic"}
+        objectFit={"contain"}
+      />
+      <Box
+        pos={"absolute"}
+        left={"50%"}
+        transform={"translateX(-50%)"}
+        h={"100%"}
+      >
+        <Flex alignItems="center" /* bg="blackAlpha.100"  */ px={3}>
+          <Text fontSize={"1.15rem"} color="white" lineHeight={".75rem"}>
+            {title}
+          </Text>
+        </Flex>
 
-        setIsLoading(true);
-
-        router.push(`product/${id}`)
-
-
-    }
-
-    return (
-        <Box mx={'auto'} bg={'transparent'} display={'flex'} justifyContent={'center'} w={'96%'} pos={'relative'}>
-            <Image
-                src={imageSrc}
-                priority={'true'}
-                placeholder={'blur'}
-                width={'568'}
-                height={'824'}
-                layout={'intrinsic'}
-                objectFit={'contain'}
-
-            />
-            <Box pos={'absolute'} left={'50%'} transform={'translateX(-50%)'} h={'100%'}>
-                <Box height={['82%']}/>
-
-                {/*<Link href={`product/${id}`}>*/}
-                <WhiteButton isLoading={isLoading} onClick={buttonClickHandler} fontWeight={'light'}
-                             justifyContent={'space-around'}
-                             p={['6px', '10px']}
-                             w={['7rem', '9rem']}
-                             size={['sm', 'md']}
-                             fontSize={['.8rem', '1rem']}
-                             variant={'solid'}
-
-                             rightIcon={<IoArrowForwardCircleOutline
-                                 size={20}
-                                 color={'dark.700'}/>}
-                >
-                    Place Bid
-                </WhiteButton>
-                {/*</Link>*/}
-            </Box>
-        </Box>
-    );
+        {/*<Link href={`product/${id}`}>*/}
+        <WhiteButton
+          isLoading={isLoading}
+          onClick={buttonClickHandler}
+          fontWeight={"light"}
+          justifyContent={"space-around"}
+          p={["6px", "10px"]}
+          w={["7rem", "9rem"]}
+          size={["sm", "md"]}
+          fontSize={[".8rem", "1rem"]}
+          variant={"solid"}
+          rightIcon={
+            <IoArrowForwardCircleOutline size={20} color={"dark.700"} />
+          }
+        >
+          Place Bid
+        </WhiteButton>
+        {/*</Link>*/}
+      </Box>
+    </Box>
+  );
 };
 
 export default ImageWithCTABtn;
